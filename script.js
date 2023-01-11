10c2aa14c485206684ae49f1e9dad131e6460a32
vikram kumar choudhary <vikrampchoudhary777@gmail.com>
2023-01-11 17:22:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+
+  var settingmenu= document.querySelector(".settings-menu");
+
+  var darkbtn= document.getElementById("dark-btn");
+function settingMenuToggle(){
+   settingmenu.classList.toggle("settings-menu-height");
+
+   if(localStorage.getItem("theme")== "light"){
+    localStorage.setItem("theme","dark");
+   }
+
+   else{
+    localStorage.setItem("theme","light");
+   }
+
+}
+
+darkbtn.onclick = function(){
+    darkbtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
+}
+
+if(localStorage.getItem("theme")=="light"){
+    darkbtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme");
+}
+
+else if(localStorage.getItem("theme")=="dark"){
+    darkbtn.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme");
+
+}
+
+else{
+     
+    localStorage.getItem("theme");
+}
+
+
+//localStorage.setItem("theme","dark");
